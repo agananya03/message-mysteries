87df9a7056ea8e16000e9bb324929dd0d2521b21
agananya03 <agananya54126@gmail.com>
2025-09-27 20:32:21 +0530

    add

--- src/helpers/sendVerificationemail.ts
+++ src/helpers/sendVerificationemail.ts
@@ -1,2 +1,16 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmails";
+import { ApiResponse } from "@/types/ApiResponse";
 
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        
+    } catch (emailError) {
+        console.log("Error sending verification email", emailError)
+        return {success: false, message: "failed to send verification email"}
+    }
+}
