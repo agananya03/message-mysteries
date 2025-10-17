76cd71947a131f5fc62f7ce332ebd0579142a94a
agananya03 <agananya54126@gmail.com>
2025-10-17 17:56:09 +0530

    init

--- src/app/api/unique-username-check/route.ts
+++ src/app/api/unique-username-check/route.ts
@@ -1,8 +1,25 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { z } from "zod" 
+import { success, z } from "zod" 
 import { usernameValidation } from "@/schemas/signUpSchema";
 
 const UsernameQuerySchema = z.object({
     username: usernameValidation
 })
+
+export async function GET(request: Request) {
+    await dbConnect()
+    try {
+        const { searchParams } = new URL(request.url)
+        const queryParams = {
+            username: searchParams.get('username')
+        }
+    } catch (error) {
+        console.error("ERror checking username", error)
+        return Response.json({
+            success: false,
+            message: "ERror checking username"
+        },
+    {status: 500})
+    }
+}
