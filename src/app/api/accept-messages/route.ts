1d434f4ca22300fe6bee32d71d5df3df7311f823
agananya03 <agananya54126@gmail.com>
2025-10-18 16:05:23 +0530

    init

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,12 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+    await dbConnect()
+    const session = await getServerSession(authOptions)
+    const user = session?.user
+    
+}
